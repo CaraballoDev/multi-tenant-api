3f9dec547e1941a397b476ba601c168b0c8fc16a
Cristian Caraballo <caraballodev@gmail.com>
2023-01-28 20:30:26 -0400

    first commit

---
+++ index.js
@@ -0,0 +1,25 @@
+
+const app = require('express')()
+const cookieParser = require('cookie-parser')
+
+app.use((req, res, next) =>
+  cors({
+    origin: process.env.CORS_ORIGIN?.split(",") ?? req.headers.origin,
+  })(req, res, next)
+);
+
+app.use(cookieParser())
+
+
+app.all('*', (req, res, next) => {
+    console.log(req.cookies)
+
+    res.json({
+        origin: req.headers.origin,
+        cookies: req.cookies
+    })
+})
+
+app.listen(process.env.PORT || 3000, function() {
+    console.log(`Server listening on port ${process.env.PORT || 3000}`)
+})
